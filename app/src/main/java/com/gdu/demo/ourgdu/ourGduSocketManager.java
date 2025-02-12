4e9681835dc6bd4cb17b7180c622ca23ddce00bd
shikuamgwei <3522922624@qq.com>
2025-02-12 09:31:10 +0800

    重写GdusocketManager

--- app/src/main/java/com/gdu/demo/ourgdu/ourGduSocketManager.java
+++ app/src/main/java/com/gdu/demo/ourgdu/ourGduSocketManager.java
@@ -1,13 +1,8 @@
 package com.gdu.demo.ourgdu;
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
-import android.util.Log;
 
 import com.gdu.common.GlobalVariable;
 import com.gdu.gdusocket.GduCommunication3;
+import com.gdu.demo.ourgdu.ourGduCommunication3;
 import com.gdu.gdusocket.GduSocketManager;
 import com.gdu.gdusocket.ce.GduCESocket3;
 import com.gdu.gdusocket.ce.IGduSocket;
@@ -16,11 +11,11 @@ import com.gdu.util.logs.RonLog2FileApi;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
-public class ourGduSocketManager{
+public class ourGduSocketManager {
     public static final String a = "GduSocketManager";
     private static ourGduSocketManager b;
     private GduCESocket3 c;
-    private GduCommunication3 d;
+    private ourGduCommunication3 d;
 
     public static ourGduSocketManager getInstance() {
         if (b == null) {
@@ -30,7 +25,7 @@ public class ourGduSocketManager{
         return b;
     }
 
-    private ourGduSocketManager(){
+    private ourGduSocketManager() {
         this.a();
     }
 
@@ -40,11 +35,12 @@ public class ourGduSocketManager{
         ourGduSocketManager var10002 = this;
         RonLog2FileApi.getSingle().saveData("api初始化Socket");
         RonLog.LogD(new String[]{"api初始化Socket"});
-        Log.d("newCall", "初始化ourSocket");
         this.c();
-        GduCESocket3 var1;
-        var10002.c = var1 = new GduCESocket3();
-        var10001.d = new GduCommunication3(var1);
+        GduCESocket3 var1=new GduCESocket3();
+        GduCESocket3 var10003 = new GduCESocket3();
+        //var10003.<init>();
+        var10002.c = var10003;
+        var10001.d = new ourGduCommunication3(var1);
         var10000.b();
     }
 
@@ -68,7 +64,7 @@ public class ourGduSocketManager{
         GlobalVariable.UDP_SOCKET_IMG_PORT = 7078;
     }
 
-    public GduCommunication3 getCommunication() {
+    public ourGduCommunication3 getCommunication() {
         return this.d;
     }
 
